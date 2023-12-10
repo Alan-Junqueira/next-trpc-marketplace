ed919bd212982a2458d094a5937384fdebe072c0
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-12-10 19:59:36 -0300

    feat: start to setup email

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,10 +1,21 @@
 import dotenv from "dotenv";
+import { createTransport } from "nodemailer";
 import path from "path";
 import payload, { Payload } from "payload";
 import type { InitOptions } from "payload/config";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
+});
+
+const transporter = createTransport({
+  host: "smtp.resend.com",
+  secure: true,
+  port: 456,
+  auth: {
+    user: "resend",
+    pass: process.env.RESEND_API_KEY,
+  },
 });
 
 let cached = (global as any).payload;
@@ -20,7 +31,9 @@ interface IArgs {
   initOptions?: Partial<InitOptions>;
 }
 
-export const getPayloadClient = async ({ initOptions }: IArgs = {}): Promise<Payload> => {
+export const getPayloadClient = async ({
+  initOptions,
+}: IArgs = {}): Promise<Payload> => {
   if (!process.env.PAYLOAD_SECRET) {
     throw new Error("PAYLOAD_SECRET is missing");
   }
@@ -31,6 +44,11 @@ export const getPayloadClient = async ({ initOptions }: IArgs = {}): Promise<Pay
 
   if (!cached.promise) {
     cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromAddress: "onboarding@resend.dev",
+        fromName: "marketplace",
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {}),
