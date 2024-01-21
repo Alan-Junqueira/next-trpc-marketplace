e8448bcde7a8b9eafd7f2035256fc6ee4f721e40
alan-junqueira <contato.alanjunqueira@gmail.com>
2024-01-21 18:28:02 -0300

    feat: finalize image slider

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,10 +1,13 @@
 import { cn } from "@/lib/utils";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { ComponentProps } from "react";
+import { ComponentProps, useEffect, useState } from "react";
+import type SwiperType from "swiper";
 import Image from "next/image";
+import { Pagination } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/pagination";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 interface IImageSliderProps extends ComponentProps<"div"> {
   urls: string[];
@@ -15,9 +18,26 @@ export const ImageSlider = ({
   className,
   ...props
 }: IImageSliderProps) => {
+  const [swiper, setSwiper] = useState<null | SwiperType>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [slideConfig, setSlideConfig] = useState({
+    isBeginning: true,
+    isEnd: activeIndex === (urls.length ?? 0) - 1,
+  });
+
   const activeStyles =
     "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
   const inactiveStyles = " hidden text-gray-400";
+
+  useEffect(() => {
+    swiper?.on("slideChange", ({ activeIndex }) => {
+      setActiveIndex(activeIndex);
+      setSlideConfig({
+        isBeginning: activeIndex === 0,
+        isEnd: activeIndex === (urls.length ?? 0) - 1,
+      });
+    });
+  }, [swiper, urls]);
   return (
     <div
       className={cn(
@@ -27,22 +47,60 @@ export const ImageSlider = ({
       {...props}
     >
       <div className="absolute inset-0 z-10 opacity-0 transition group-hover:opacity-100">
-        <button></button>
-        <button></button>
-        <Swiper className="h-full w-full">
-          {urls.map((url, index) => (
-            <SwiperSlide key={index} className="relative -z-10 h-full w-full">
-              <Image
-                fill
-                loading="eager"
-                src={url}
-                className="-z-10 h-full w-full object-cover object-center"
-                alt="Product Image"
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slideNext();
+          }}
+          className={cn(activeStyles, "right-3 transition", {
+            [inactiveStyles]: slideConfig.isEnd,
+            "hover:bg-primary-300 text-primary-800 opacity-100":
+              !slideConfig.isEnd,
+          })}
+          aria-label="next image"
+        >
+          <ChevronRight className="h-4 w-4 text-zinc-700" />{" "}
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slidePrev();
+          }}
+          className={cn(activeStyles, "left-3 transition", {
+            [inactiveStyles]: slideConfig.isBeginning,
+            "hover:bg-primary-300 text-primary-800 opacity-100":
+              !slideConfig.isBeginning,
+          })}
+          aria-label="previous image"
+        >
+          <ChevronLeft className="h-4 w-4 text-zinc-700" />{" "}
+        </button>
       </div>
+
+      <Swiper
+        pagination={{
+          renderBullet: (_, className) => {
+            return `<span class="rounded-full transition ${className}"></span>`;
+          },
+        }}
+        onSwiper={(swiper) => setSwiper(swiper)}
+        spaceBetween={50}
+        modules={[Pagination]}
+        slidesPerView={1}
+        className="h-full w-full"
+      >
+        {urls.map((url, i) => (
+          <SwiperSlide key={i} className="relative -z-10 h-full w-full">
+            <Image
+              fill
+              loading="eager"
+              className="-z-10 h-full w-full object-cover object-center"
+              src={url}
+              alt="Product image"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
